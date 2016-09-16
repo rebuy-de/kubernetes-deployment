58539748fb518fbc7f7fc8c78d53529b5ebaca60
Florian Zeidler <f.zeidler@rebuy.com>
2016-09-16 13:56:48 +0200

    CLOUD-352: Make it OS independent

--- hack/build.sh
+++ hack/build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-REPO=github.com/rebuy-de/kubernetes-deployment
-ROOT=$(readlink -f $( dirname $0)/..)
-COMMAND=${1:-all}
-
 set -ex
+REPO=github.com/rebuy-de/kubernetes-deployment
+ROOT=$(dirname $(dirname $0))
+COMMAND=${1:-all}
 
 docker run \
 	--rm \
-	-v "${ROOT}:/go/src/${REPO}" \
+	-v "${PWD}:/go/src/${REPO}" \
 	074509403805.dkr.ecr.eu-west-1.amazonaws.com/rebuy-base-image-golang:latest \
     /tools/build.sh ${COMMAND} ${REPO}
