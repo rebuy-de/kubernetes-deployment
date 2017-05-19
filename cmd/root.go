e4712f354a073d4bf499604644fe1ea246f8d5b3
Sven Walter <s.walter@rebuy.com>
2017-05-19 12:24:33 +0200

    make parameters global

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -11,14 +13,26 @@ func NewRootCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "kubernetes-deployment",
 		Short: "Manages deployments to our Kubernetes cluster",
-		PersistentPreRun: func(cmd *cobra.Command, args []string) {
-			log.SetLevel(log.DebugLevel)
-			log.SetOutput(os.Stdout)
-		},
 	}
 
 	params := new(Parameters)
 	params.Bind(cmd)
+
+	cmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
+		log.SetLevel(log.DebugLevel)
+		log.SetOutput(os.Stdout)
+
+		err := params.ReadIn()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if strings.TrimSpace(params.Filename) == "" {
+			return fmt.Errorf("You have to specify a filename.")
+		}
+
+		return nil
+	}
 
 	cmd.AddCommand(NewVersionCommand())
 	cmd.AddCommand(NewDeployCommand(params))
