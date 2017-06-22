9709d79236640c802c2e204ce0b3fd21d941dee3
Sven Walter <s.walter@rebuy.com>
2017-06-22 16:59:53 +0200

    initialize seed

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
+	"time"
 
 	"github.com/rebuy-de/kubernetes-deployment/cmd"
 )
+
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
 
 func main() {
 	if err := cmd.NewRootCommand().Execute(); err != nil {
