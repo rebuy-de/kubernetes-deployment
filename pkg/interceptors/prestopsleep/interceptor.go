c3d721bcd9bd285aacde28661ef20d55cbd5ab7b
Sven Walter <s.walter@rebuy.com>
2018-08-10 14:24:22 +0200

    update interceptors

--- pkg/interceptors/prestopsleep/interceptor.go
+++ pkg/interceptors/prestopsleep/interceptor.go
@@ -5,10 +5,13 @@ import (
 	"reflect"
 
 	log "github.com/sirupsen/logrus"
-	v1beta1apps "k8s.io/api/apps/v1beta1"
 	"k8s.io/api/core/v1"
-	v1beta1extensions "k8s.io/api/extensions/v1beta1"
 	"k8s.io/apimachinery/pkg/runtime"
+
+	v1apps "k8s.io/api/apps/v1"
+	v1beta1apps "k8s.io/api/apps/v1beta1"
+	v1beta2apps "k8s.io/api/apps/v1beta2"
+	v1beta1extensions "k8s.io/api/extensions/v1beta1"
 )
 
 type Interceptor struct {
@@ -23,19 +26,33 @@ func New(sleepSeconds int) *Interceptor {
 
 func (i *Interceptor) PostManifestRender(obj runtime.Object) (runtime.Object, error) {
 	switch typed := obj.(type) {
-	case *v1beta1extensions.Deployment:
-		i.AddToDeployment(typed)
-		return typed, nil
+	case *v1apps.StatefulSet:
+		i.AddToPodTemplate(&typed.Spec.Template)
+
+	case *v1beta2apps.StatefulSet:
+		i.AddToPodTemplate(&typed.Spec.Template)
 
 	case *v1beta1apps.StatefulSet:
-		i.AddToStatefulSet(typed)
-		return typed, nil
+		i.AddToPodTemplate(&typed.Spec.Template)
+
+	case *v1apps.Deployment:
+		i.AddToPodTemplate(&typed.Spec.Template)
+
+	case *v1beta2apps.Deployment:
+		i.AddToPodTemplate(&typed.Spec.Template)
+
+	case *v1beta1apps.Deployment:
+		i.AddToPodTemplate(&typed.Spec.Template)
+
+	case *v1beta1extensions.Deployment:
+		i.AddToPodTemplate(&typed.Spec.Template)
 
 	default:
 		log.WithFields(log.Fields{
 			"type": reflect.TypeOf(obj),
-		}).Debug("type doesn't support adding preStop hook")
+		}).Debug("type doesn't support removal of resource specs")
 	}
+
 	return obj, nil
 }
 
@@ -55,16 +72,8 @@ func (i *Interceptor) AddToContainer(container *v1.Container) {
 	}
 }
 
-func (i *Interceptor) AddToPodTemplace(tpl *v1.PodTemplateSpec) {
+func (i *Interceptor) AddToPodTemplate(tpl *v1.PodTemplateSpec) {
 	for j := range tpl.Spec.Containers {
 		i.AddToContainer(&tpl.Spec.Containers[j])
 	}
-}
-
-func (i *Interceptor) AddToDeployment(deployment *v1beta1extensions.Deployment) {
-	i.AddToPodTemplace(&deployment.Spec.Template)
-}
-
-func (i *Interceptor) AddToStatefulSet(set *v1beta1apps.StatefulSet) {
-	i.AddToPodTemplace(&set.Spec.Template)
 }
--- pkg/interceptors/rmresspec/interceptor.go
+++ pkg/interceptors/rmresspec/interceptor.go
@@ -4,10 +4,13 @@ import (
 	"reflect"
 
 	log "github.com/sirupsen/logrus"
-	v1beta1apps "k8s.io/api/apps/v1beta1"
 	"k8s.io/api/core/v1"
-	v1beta1extensions "k8s.io/api/extensions/v1beta1"
 	"k8s.io/apimachinery/pkg/runtime"
+
+	v1apps "k8s.io/api/apps/v1"
+	v1beta1apps "k8s.io/api/apps/v1beta1"
+	v1beta2apps "k8s.io/api/apps/v1beta2"
+	v1beta1extensions "k8s.io/api/extensions/v1beta1"
 )
 
 type Interceptor struct {
@@ -19,21 +22,37 @@ func New() *Interceptor {
 
 func (i *Interceptor) PostManifestRender(obj runtime.Object) (runtime.Object, error) {
 	switch typed := obj.(type) {
-	case *v1beta1extensions.Deployment:
-		return RemoveFromDeployment(typed), nil
+	case *v1apps.StatefulSet:
+		RemoveFromPodTemplate(typed.Spec.Template)
+
+	case *v1beta2apps.StatefulSet:
+		RemoveFromPodTemplate(typed.Spec.Template)
 
 	case *v1beta1apps.StatefulSet:
-		return RemoveFromStatefulSet(typed), nil
+		RemoveFromPodTemplate(typed.Spec.Template)
+
+	case *v1apps.Deployment:
+		RemoveFromPodTemplate(typed.Spec.Template)
+
+	case *v1beta2apps.Deployment:
+		RemoveFromPodTemplate(typed.Spec.Template)
+
+	case *v1beta1apps.Deployment:
+		RemoveFromPodTemplate(typed.Spec.Template)
+
+	case *v1beta1extensions.Deployment:
+		RemoveFromPodTemplate(typed.Spec.Template)
 
 	default:
 		log.WithFields(log.Fields{
 			"type": reflect.TypeOf(obj),
 		}).Debug("type doesn't support removal of resource specs")
 	}
+
 	return obj, nil
 }
 
-func RemoveFromPodTemplate(tpl v1.PodTemplateSpec) v1.PodTemplateSpec {
+func RemoveFromPodTemplate(tpl v1.PodTemplateSpec) {
 	for i := range tpl.Spec.Containers {
 		tpl.Spec.Containers[i].Resources = v1.ResourceRequirements{}
 	}
@@ -41,16 +60,4 @@ func RemoveFromPodTemplate(tpl v1.PodTemplateSpec) v1.PodTemplateSpec {
 	for i := range tpl.Spec.InitContainers {
 		tpl.Spec.InitContainers[i].Resources = v1.ResourceRequirements{}
 	}
-
-	return tpl
-}
-
-func RemoveFromDeployment(deployment *v1beta1extensions.Deployment) *v1beta1extensions.Deployment {
-	deployment.Spec.Template = RemoveFromPodTemplate(deployment.Spec.Template)
-	return deployment
-}
-
-func RemoveFromStatefulSet(set *v1beta1apps.StatefulSet) *v1beta1apps.StatefulSet {
-	set.Spec.Template = RemoveFromPodTemplate(set.Spec.Template)
-	return set
 }
