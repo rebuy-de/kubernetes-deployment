1b644ac1d76c79c0425e90ee3a9e9efc5bd3fdbf
Sven Walter <s.walter@rebuy.com>
2019-02-22 11:41:11 +0100

    move annotation command to separate function

--- pkg/interceptors/annotater/interceptor.go
+++ pkg/interceptors/annotater/interceptor.go
@@ -1,11 +1,13 @@
 package annotater
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/benbjohnson/clock"
 	"github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/api/meta"
+	v1meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 
 	"github.com/rebuy-de/kubernetes-deployment/pkg/gh"
@@ -13,19 +15,20 @@ import (
 
 type Interceptor struct {
 	clock    clock.Clock
+	now      time.Time
 	branch   gh.Branch
 	timezone *time.Location
 }
 
 func New() *Interceptor {
 	return &Interceptor{
-		clock:    clock.New(),
-		timezone: time.Local,
+		clock: clock.New(),
 	}
 }
 
 func (i *Interceptor) PostFetch(branch *gh.Branch) error {
 	i.branch = *branch
+	i.now = i.clock.Now().In(i.timezone)
 	return nil
 }
 
@@ -35,28 +38,34 @@ func (i *Interceptor) PostManifestRender(obj runtime.Object) (runtime.Object, er
 		return nil, err
 	}
 
-	now := i.clock.Now()
+	i.annotate(accessor, accessor)
 
-	annotations := accessor.GetAnnotations()
+	return obj, nil
+}
+
+func (i *Interceptor) annotate(owner, obj v1meta.Object) {
+	key := func(n string) string { return fmt.Sprintf("rebuy.com/kubernetes-deployment.%s", n) }
+
+	annotations := obj.GetAnnotations()
 	if annotations == nil {
 		annotations = map[string]string{}
 	}
 
-	annotations["rebuy.com/kubernetes-deployment.deployment-date"] = now.In(i.timezone).Format(time.RFC3339Nano)
-	annotations["rebuy.com/kubernetes-deployment.commit-sha"] = i.branch.SHA
-	annotations["rebuy.com/kubernetes-deployment.commit-date"] = i.branch.Date.In(i.timezone).Format(time.RFC3339Nano)
-	annotations["rebuy.com/kubernetes-deployment.commit-author"] = i.branch.Author
-	annotations["rebuy.com/kubernetes-deployment.commit-message"] = i.branch.Message
-	annotations["rebuy.com/kubernetes-deployment.commit-location"] = i.branch.Location.String()
+	annotations[key("deployment-date")] = i.now.Format(time.RFC3339Nano)
+	annotations[key("commit-sha")] = i.branch.SHA
+	annotations[key("commit-date")] = i.branch.Date.In(i.timezone).Format(time.RFC3339Nano)
+	annotations[key("commit-author")] = i.branch.Author
+	annotations[key("commit-message")] = i.branch.Message
+	annotations[key("commit-location")] = i.branch.Location.String()
 
-	accessor.SetAnnotations(annotations)
+	obj.SetAnnotations(annotations)
 
-	labels := accessor.GetLabels()
+	labels := obj.GetLabels()
 	if labels == nil {
 		labels = map[string]string{}
 	}
 
-	name := accessor.GetName()
+	name := owner.GetName()
 	labelName, ok := labels["name"]
 	if ok && name != labelName {
 		logrus.Warnf("Existing label name '%s' not match manifest name '%s'",
@@ -65,7 +74,5 @@ func (i *Interceptor) PostManifestRender(obj runtime.Object) (runtime.Object, er
 		labels["name"] = name
 	}
 
-	accessor.SetLabels(labels)
-
-	return obj, nil
+	obj.SetLabels(labels)
 }
