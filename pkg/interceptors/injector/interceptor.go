86e2d212c0b766970e360b9aafde88e0f0096ee9
Björn Häuser <b.haeuser@rebuy.com>
2019-07-02 08:10:57 +0200

    Change linkerd proxy configuration to set outbound connect timeout
    * Add a way to easily change the linkerd-proxy sidecar
    * Add a general test for linkerd injection

--- pkg/interceptors/injector/interceptor.go
+++ pkg/interceptors/injector/interceptor.go
@@ -3,6 +3,7 @@ package injector
 import (
 	"bytes"
 	"encoding/json"
+	"k8s.io/api/core/v1"
 	"os"
 	"os/exec"
 
@@ -14,10 +15,14 @@ import (
 	"github.com/rebuy-de/kubernetes-deployment/pkg/kubeutil"
 )
 
-type Interceptor struct{}
+type Interceptor struct {
+	Options Options
+}
 
-func New() *Interceptor {
-	return &Interceptor{}
+func New(options Options) *Interceptor {
+	return &Interceptor{
+		Options: options,
+	}
 }
 
 func (i *Interceptor) PostManifestRender(obj runtime.Object) (runtime.Object, error) {
@@ -41,11 +46,9 @@ func (i *Interceptor) PostManifestRender(obj runtime.Object) (runtime.Object, er
 		return nil, errors.WithStack(err)
 	}
 
-	cmd := exec.Command(
-		"linkerd", "inject",
-		"--proxy-memory-request", "20Mi",
-		"--proxy-cpu-request", "35m",
-		"-")
+	args := append([]string{"inject"}, append(i.Options.InjectArguments, "-")...)
+	cmd := exec.Command("linkerd", args...)
+
 	cmd.Stdin = bytes.NewBuffer(marshalled)
 	cmd.Stderr = os.Stderr
 	newUnmarshalled, err := cmd.Output()
@@ -54,5 +57,28 @@ func (i *Interceptor) PostManifestRender(obj runtime.Object) (runtime.Object, er
 	}
 
 	newObj, err := kubeutil.Decode(newUnmarshalled)
+
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to decode result json")
+	}
+
+	template := kubeutil.PodTemplateSpecAccessor(newObj)
+	if template != nil {
+		for _, c := range template.Spec.Containers {
+			if c.Name != "linkerd-proxy" {
+				continue
+			}
+
+			i.addLinkerdEnvVariables(c)
+		}
+	}
+
 	return newObj, errors.Wrapf(err, "failed to decode result json")
+}
+
+func (i *Interceptor) addLinkerdEnvVariables(c v1.Container) {
+	c.Env = append(c.Env, v1.EnvVar{
+		Name:  "LINKERD2_PROXY_OUTBOUND_CONNECT_TIMEOUT",
+		Value: i.Options.ConnectTimeout,
+	})
 }
